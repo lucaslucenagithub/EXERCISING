0ae1cac693b94e4f323809ef0e881978f1bcece4
Lucas Lucena <dayyumn@gmail.com>
2021-04-10 18:59:08 -0300

    amazon recursion functions studied

---
+++ Amazon/Recursion/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static int factorial(int n)
+        {
+            if (n >= 1) { var val = n * factorial(n - 1); return val; }
+            else { return 1; };
+        }
+
+        private static int fibonacci(int position)
+        {
+            if (position == 1 || position == 2) { return 1; }
+            var positionValue = fibonacci(position - 1) + fibonacci(position - 2);
+            return positionValue;
+        }
+
+        static void Main(string[] args)
+        {
+            //var result = factorial(4);
+            var result = fibonacci(8);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
